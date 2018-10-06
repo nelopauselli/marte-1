935d6b39a613f688237263e3dfafa7952d619ca3
Nelo Pauselli <nelopauselli@gmail.com>
2018-10-06 15:44:26 -0300

    programando botones y luego ejecutando la secuencia programada

--- firmware/src/Motor.cpp
+++ firmware/src/Motor.cpp
@@ -3,61 +3,38 @@
 #ifndef MOTOR_CLASS
 #define MOTOR_CLASS
 
-#define MAX_STEPS 4
-
 class Motor
 {
   public:
-    Motor(int in1, int in2, int in3, int in4)
+    Motor(int in1, int in2)
     {
         this->in1 = in1;
         this->in2 = in2;
-        this->in3 = in3;
-        this->in4 = in4;
 
         pinMode(this->in1, OUTPUT);
         pinMode(this->in2, OUTPUT);
-        pinMode(this->in3, OUTPUT);
-        pinMode(this->in4, OUTPUT);
-
-        step = 0;
     }
 
     void foward()
     {
-        step++;
-        if (step > MAX_STEPS - 1)
-            step = 0;
-            move();
+        digitalWrite(this->in2, LOW);
+        digitalWrite(this->in1, HIGH);
     }
 
     void back()
     {
-        step--;
-        if (step < 0)
-            step = MAX_STEPS - 1;
-        move();
+        digitalWrite(this->in1, LOW);
+        digitalWrite(this->in2, HIGH);
+    }
+
+    void stop()
+    {
+        digitalWrite(this->in1, LOW);
+        digitalWrite(this->in2, LOW);
     }
 
   private:
-    short steps[MAX_STEPS] = {0b00000001, 0b00000010, 0b00000100, 0b00001000};
-    int in1, in2, in3, in4;
-    int step;
-
-    void move()
-    {
-        const short &current = steps[step % MAX_STEPS];
-        digitalWrite(in1, current & 0b00000001);
-        digitalWrite(in2, current & 0b00000010);
-        digitalWrite(in3, current & 0b00000100);
-        digitalWrite(in4, current & 0b00001000);
-
-        Serial.print(current & 0b00000001 ? '1' : '0');
-        Serial.print(current & 0b00000010 ? '1' : '0');
-        Serial.print(current & 0b00000100 ? '1' : '0');
-        Serial.print(current & 0b00001000 ? '1' : '0');
-        Serial.println();
-    }
+    int in1, in2;
 };
 
 #endif
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,70 +1,178 @@
 #include <Arduino.h>
 #include "Motor.cpp"
 
+#define BUTTON_1 11
+#define BUTTON_2 10
+#define BUTTON_3 12
+
+#define NONE 0
 #define FOWARD 1
 #define RIGHT 2
-#define BACK 3
-#define LEFT 4
-int direction = FOWARD;
+#define LEFT 3
+#define BACK 4
 
-Motor motorDerecho(5, 4, 3, 2);
-Motor motorIzquierdo(9, 8, 7, 6);
+int direction = NONE;
+
+Motor motorDerecho(2, 3);
+Motor motorIzquierdo(5, 4);
+
+#define MODE_NONE 0
+#define MODE_PROG 1
+#define MODE_RUN 2
+#define MODE_TEST 3
+
+int mode = MODE_NONE;
+
+#define COMMANDS_SIZE 4
+int commands[COMMANDS_SIZE];
+int current = 0;
+
+void blink(int speed = 500)
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(speed / 2);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(speed / 2);
+}
+
+void setMode(int value)
+{
+    mode = value;
+    current = 0;
+
+    if (mode == MODE_PROG)
+    {
+        // blink(200);
+        // blink(200);
+        // blink(200);
+    }
+    else if (mode == MODE_RUN)
+    {
+        blink(500);
+        blink(400);
+        blink(300);
+        blink(200);
+        blink(100);
+    }
+}
 
 void setup()
 {
-    Serial.begin(115200);
-    // delay(1000);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-    Serial.println("Motores Inicializados");
+    pinMode(BUTTON_1, INPUT_PULLUP);
+    pinMode(BUTTON_2, INPUT_PULLUP);
+    pinMode(BUTTON_3, INPUT_PULLUP);
+
+    //test();
+
+    setMode(MODE_PROG);
 }
 
-int steps = 0;
-void loop()
+void tester()
 {
-    switch (direction)
+    motorDerecho.stop();
+    motorIzquierdo.stop();
+
+    delay(1000);
+    motorIzquierdo.foward();
+    delay(1000);
+    motorIzquierdo.stop();
+
+    delay(1000);
+    motorIzquierdo.back();
+    delay(1000);
+    motorIzquierdo.stop();
+
+    delay(1000);
+    motorDerecho.foward();
+    delay(1000);
+    motorDerecho.stop();
+
+    delay(1000);
+    motorDerecho.back();
+    delay(1000);
+    motorDerecho.stop();
+}
+
+void programmer()
+{
+    if (digitalRead(BUTTON_1) == LOW)
+    {
+        commands[current] = FOWARD;
+        current++;
+        blink();
+    }
+    else if (digitalRead(BUTTON_2) == LOW)
+    {
+        commands[current] = LEFT;
+        current++;
+        blink();
+    }
+    else if (digitalRead(BUTTON_3) == LOW)
+    {
+        commands[current] = RIGHT;
+        current++;
+        blink();
+    }
+
+    if (current == COMMANDS_SIZE)
+    {
+        setMode(MODE_RUN);
+    }
+}
+
+void runner()
+{
+    byte command = commands[current++];
+
+    switch (command)
     {
     case FOWARD:
         motorDerecho.foward();
         motorIzquierdo.foward();
         break;
     case LEFT:
+        motorIzquierdo.stop();
         motorDerecho.foward();
-        motorIzquierdo.back();
+        break;
+    case RIGHT:
+        motorDerecho.stop();
+        motorIzquierdo.foward();
         break;
     case BACK:
         motorDerecho.back();
         motorIzquierdo.back();
         break;
-    case RIGHT:
-        motorDerecho.back();
-        motorIzquierdo.foward();
-        break;
     }
 
-    delay(3);
-
-    steps++;
-    if (steps > 2048)
+    if (current == COMMANDS_SIZE)
     {
-        steps = 0;
-        switch (direction)
-        {
-        case FOWARD:
-            direction = BACK;
-            Serial.println("Back");
-            break;
-        case BACK:
-            direction = LEFT;
-            Serial.println("Left");
-            break;
-        case LEFT:
-            direction = RIGHT;
-            Serial.println("Right");
-            break;
-        case RIGHT:
-            direction = FOWARD;
-            Serial.println("Foward");
-            break;
-        }
+        current = 0;
+        setMode(MODE_PROG);
+
+        motorDerecho.stop();
+        motorIzquierdo.stop();
     }
+    else
+    {
+        delay(1000);
+    }
+}
+
+void loop()
+{
+    if (mode == MODE_PROG)
+    {
+        programmer();
+    }
+    else if (mode == MODE_RUN)
+    {
+        runner();
+    }
+    else if (mode == MODE_TEST)
+    {
+        tester();
+    }
+    return;
 }
